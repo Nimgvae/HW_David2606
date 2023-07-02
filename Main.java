6afa6b016249a18875fc95812f08d2d5ff09eaff
Eugen Korschun <117730070+Nimgvae@users.noreply.github.com>
2023-07-02 19:13:56 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,61 @@
+package HW_2606;
+/*
+* Имея два отсортированных массива размера m и n соответственно, вам нужно найти элемент, который будет находиться на k-й позиции в конечном отсортированном массиве.
+
+Массив 1 - 100 112 256 349 770
+Массив 2 - 72 86 113 119 265 445 892
+к = 7
+Вывод : 256
+
+
+Окончательный отсортированный массив -
+72, 86, 100, 112, 113, 119, 256, 265, 349, 445, 770, 892
+7-й элемент этого массива равен 256.
+* */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array1 = {100, 112, 256, 349, 770};
+        int[] array2 = {72, 86, 113, 119, 265, 445, 892};
+        int k = 7;
+
+        int[] mergedArray = mergeArrays(array1, array2);
+        int kthElement = findKthElement(mergedArray, k);
+
+        System.out.println("Отсортированный массив:");
+        for (int num : mergedArray) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\n" + k + "-й элемент: " + kthElement);
+    }
+
+    public static int[] mergeArrays(int[] array1, int[] array2) {
+        int[] mergedArray = new int[array1.length + array2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] <= array2[j]) {
+                mergedArray[k++] = array1[i++];
+            } else {
+                mergedArray[k++] = array2[j++];
+            }
+        }
+
+        while (i < array1.length) {
+            mergedArray[k++] = array1[i++];
+        }
+
+        while (j < array2.length) {
+            mergedArray[k++] = array2[j++];
+        }
+
+        return mergedArray;
+    }
+
+    public static int findKthElement(int[] mergedArray, int k) {
+        return mergedArray[k - 1];
+    }
+
+}
+
